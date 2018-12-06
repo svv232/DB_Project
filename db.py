58b5800d5d68c4daf7e578552d7ce72e073ed22c
Roy Xu <captiosus@users.noreply.github.com>
2018-12-06 02:01:41 -0500

    stylistic changes

--- db.py
+++ db.py
@@ -132,33 +132,36 @@ def get_my_content_ids(email):
 def count_tags(item_id):
     cursor = conn.cursor()
     query = ("SELECT COUNT(*) FROM TAG WHERE item_id=%s")
-    query.execute(query,(item_id))
+    query.execute(query, (item_id))
     content = cursor.fetchall()
     cursor.close()
     return content, f"found tag number for item_id {item_id}"
 
+
 def get_tags_from_item_id(item_id):
     cursor = conn.cursor()
     query = ("SELECT * FROM TAG WHERE item_id=%s")
-    query.execute(query,(item_id))
+    query.execute(query, (item_id))
     content = cursor.fetchall()
     cursor.close()
     return content, f"found tags for item_id {item_id}"
 
+
 def get_friend_group_by_primkey(fg_name, email):
     cursor = conn.cursor()
-    query = ("SELECT fg_name, email FROM Friendgroup where "+
-            "owner_email=%s AND fg_name=%s")
+    query = ("SELECT fg_name, email FROM Friendgroup where "
+             "owner_email=%s AND fg_name=%s")
     cursor.execute(query, (email, fg_name))
     content = cursor.fetchall()
     cursor.close()
     return content, "Successfully found friendgroup from primary keys"
 
+
 def accept_tag_on_content_item(email_tagged, email_tagger, item_id):
     cursor = conn.cursor()
     query = ("UPDATE Tag SET status=TRUE WHERE email_tagged=%s AND"
-            "email_tagger=%s AND item_id=%s")
-    query.execute(query,(email_tagged, email_tagger, item_id))
+             "email_tagger=%s AND item_id=%s")
+    query.execute(query, (email_tagged, email_tagger, item_id))
     cursor.close()
     return True, "Successfully updated tag on content item"
 
@@ -170,10 +173,6 @@ def remove_tag_on_content_item(email_tagged, email_tagger, item_id):
     query.execute(query, (email_tagged, email_tagger, item_id))
     cursor.close()
     return True, 'Successfully deleted content item'
-
-
-def accept_tag_on_content_item(email_tagged, email_tagger, item_id):
-    pass
 
 
 def tag_content_item(email_tagged, email_tagger, item_id):
