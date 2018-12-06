a18e2da8aa47c16f1e74e3d1c2f87b97a632abe1
Sai <svv232@nyu.edu>
2018-12-06 01:45:01 -0500

    pushed more stuff

--- db.py
+++ db.py
@@ -129,7 +129,20 @@ def get_my_content_ids(email):
     return content, 'Successfully Got Content!'
 
 def count_tags(item_id):
-    pass
+    cursor = conn.cursor()
+    query = ("SELECT COUNT(*) FROM TAG WHERE item_id=%s")
+    query.execute(query,(item_id))
+    content = cursor.fetchall()
+    cursor.close()
+    return content, f"found tag number for item_id {item_id}"
+
+def get_tags_from_item_id(item_id):
+    cursor = conn.cursor()
+    query = ("SELECT * FROM TAG WHERE item_id=%s")
+    query.execute(query,(item_id))
+    content = cursor.fetchall()
+    cursor.close()
+    return content, f"found tags for item_id {item_id}"
 
 def get_friend_group_by_primkey(fg_name, email):
     cursor = conn.cursor()
